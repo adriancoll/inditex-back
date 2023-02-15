0f5bc6265b825ce3de60a84fda7057099137cab5
Adrián coll <nege2002@gmail.com>
2023-02-15 23:58:06 +0100

    feat: return all products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,7 +1,13 @@
 import { response, request } from 'express'
+import { Product } from '../models/product.entity.js'
+import apiResponses from '../helpers/api-responses.js'
 
-export const getProducts = (req = request, res = response) => {
-  return res.json(['ProductList'])
+export const getProducts = async (_req = request, res = response) => {
+  const products = await Product.find()
+
+  return res.json(
+    apiResponses.success(`Products fetched: ${products.length}`, products)
+  )
 }
 
 export const getProductDetail = (req = request, res = response) => {
