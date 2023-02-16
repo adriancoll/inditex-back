8653e42a4d2310660f3cc8afb866c7a18d32950e
Adrián coll <nege2002@gmail.com>
2023-02-16 19:50:29 +0100

    feat: product detail endpoint

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,6 +1,6 @@
 import { response, request } from 'express'
 import { Product } from '../models/product.entity.js'
-import apiResponses from '../helpers/api-responses.js'
+import apiResponses, { error, success } from '../helpers/api-responses.js'
 
 export const getProducts = async (_req = request, res = response) => {
   const products = await Product.find()
@@ -10,6 +10,12 @@ export const getProducts = async (_req = request, res = response) => {
   )
 }
 
-export const getProductDetail = (req = request, res = response) => {
-  return res.json('Product detail')
+export const getProductDetail = async (req = request, res = response) => {
+  const { id } = req.params
+
+  const product = await Product.findById(id)
+
+  if (!product) return res.json(error('Producto no encontrado'))
+
+  return res.json(success('Detalles del producto', { product }))
 }
